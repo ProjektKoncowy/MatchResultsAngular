534d682e1caa044cb69fec3793f3ab6100bb9f8c
Marek Fryger <marekfryger@o2.pl>
2022-04-09 16:54:31 +0200

    Home page start work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'new';
+  title = 'Match results';
+  private AngileskaLiga = new League('Liga Angielska', 2);
+}
+class League {
+  private name: string;
+  private competition_id: number;
+
+  constructor(name: string, competition_id: number) {
+    this.name = name;
+    this.competition_id = competition_id;
+  }
 }
---
+++ src/app/league-table/league-table.component.ts
@@ -0,0 +1,16 @@
+import { Component, Injectable, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-league-table',
+  templateUrl: './league-table.component.html',
+  styleUrls: ['./league-table.component.css']
+})
+
+export class LeagueTableComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
+
+
