8ad3f819e7cc42256c406599f4f6c47a0330fb9a
Marek_Fryger <marekfryger@o2.pl>
2022-04-27 14:48:03 +0200

    zmiana wyniku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LeagueTableComponent } from './league-table/league-table.component';
@@ -13,6 +14,7 @@ import { LeagueTableComponent } from './league-table/league-table.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+
 
   ],
   providers: [],
